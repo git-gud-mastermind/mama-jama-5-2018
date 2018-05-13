4396faba36e1b44351aa16fb5e529a842624448d
Jon Glover <juandglover@gmail.com>
2018-05-13 12:15:02 -0700

    Add basic deck scripts

---
+++ Assets/Scripts/Decks/ChaosDeck.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Chaos Deck", menuName = "Chaos Power Deck")]
+public class ChaosDeck : Deck {
+
+
+}
---
+++ Assets/Scripts/Decks/Deck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Deck", menuName = "Card Deck")]
+public class Deck : ScriptableObject {
+
+	public List<Card> deck;
+
+	/**
+	*  @function DrawCard
+	*  @description Draw a card from the deck.
+	*  @return Card - The card drawn
+	*/
+	public Card DrawCard(){
+
+	}
+}
