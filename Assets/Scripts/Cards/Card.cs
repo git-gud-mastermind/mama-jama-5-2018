f3da83a0132f4aec30018fe2c928ffbbbe63f540
Jon Glover <juandglover@gmail.com>
2018-05-15 14:43:45 -0700

    Make card class abstract again

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Card : ScriptableObject {
+public abstract class Card : ScriptableObject {
 
 	public Sprite cardArt;
 
