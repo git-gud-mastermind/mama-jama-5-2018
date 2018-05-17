43292b3e4cdd5ccce436bc06dfdbd62af51d0045
Patrick Scott <gitkraken@patrick-scott.com>
2018-05-16 22:36:59 -0700

    Server Connection, Room Find, UI Draft
    Currently will join or create a random room via cloud matchmaking. No callback functionality for when the room is filled, yet.
    
    In what will be an additive overlay scene.
    
    Matchmaking only.

---
+++ Assets/Scripts/Networking/ConnectionHandler.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectionHandler : Photon.PunBehaviour {
+
+	[SerializeField] string gameVersion = "0";
+
+	public List<Selectable> connectionReqdObject = new List<Selectable>();
+	List<bool> croStartingStates = new List<bool>();
+
+	public TextTweening infoText;
+
+	public List<string> waitingTexts = new List<string>()
+	{
+		"Waiting for an idle god",
+		".Waiting for an idle god.",
+		"..Waiting for an idle god..",
+		"...Waiting for an idle god..."
+	};
+
+	#region Unity Callbacks
+
+	private void Awake()
+	{
+		PhotonNetwork.autoJoinLobby = false;
+
+		PhotonNetwork.automaticallySyncScene = true;
+	}
+
+	private void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings(gameVersion);
+
+		foreach (var item in connectionReqdObject)
+		{	
+			croStartingStates.Add(item.interactable);
+			item.interactable = false;
+		}
+	}
+
+	#endregion
+
+
+	#region Primary Methods
+
+	/// <summary>
+	/// Start the connection process. 
+	/// - If already connected, we attempt joining a random room
+	/// - if not yet connected, Connect this application instance to Photon Cloud Network
+	/// </summary>
+	public void Connect(string ip)
+	{
+		// we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+		if (PhotonNetwork.connected)
+		{
+			//Matchmaking
+			if (string.IsNullOrEmpty(ip))
+			{
+				// #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnPhotonRandomJoinFailed() and we'll create one.
+				PhotonNetwork.JoinRandomRoom();
+			}
+			else
+			{
+				//Stub. May add Direct Connect
+			}
+		}
+
+		else
+		{
+			// #Critical, we must first and foremost connect to Photon Online Server.
+			PhotonNetwork.ConnectUsingSettings(gameVersion);
+		}
+	}
+
+	#endregion
+
+
+	#region Photon.PunBehaviour CallBacks
+
+	public override void OnConnectedToPhoton()
+	{
+		infoText.SetText("Welcome");
+
+		//Set our UI interactability to its proper starting state
+		connectionReqdObject.ForEach(obj => obj.interactable = croStartingStates [connectionReqdObject.IndexOf(obj)]);
+	}
+
+	public override void OnDisconnectedFromPhoton()
+	{
+		infoText.SetText("Disconnected.");
+	}
+
+	public override void OnPhotonRandomJoinFailed(object [] codeAndMsg)
+	{
+		// #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+		PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 2 }, null);
+	}
+
+	public override void OnJoinedRoom()
+	{
+		//Wait for match
+		if (PhotonNetwork.room.PlayerCount == 1)
+		{
+			infoText.SetTextOnLoop(waitingTexts, 0.3f);
+		}
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/TextTweening.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TextTweening : MonoBehaviour {
+
+	[SerializeField] float fadeInTime = 0.5f, fadeOutTime = 0.3f;
+
+	TMPro.TextMeshProUGUI textUI;
+	Sequence currentSequence;
+
+	/// <summary>
+	/// Directly change the text. Complete with fading in and out.
+	/// </summary>
+	/// <param name="text">The string to display in the UI</param>
+	public void SetText(string text)
+	{
+		ResetSequence()
+			.AppendCallback(() => textUI.text = text)
+			.Append(textUI.DOFade(1, fadeInTime))
+			.OnKill(() => currentSequence = null);	//Clear the in-class ref so we don't throw logs in console
+	}
+
+	/// <summary>
+	/// Animate the text through a series of strings. Ends on last string.
+	/// </summary>
+	/// <param name="texts">The strings, in order, to display in the UI</param>
+	/// <param name="sequenceDelay">Time between each string, unfaded</param>
+	public void SetTextInSequence(List<string> texts, float sequenceDelay)
+	{
+		ResetSequence();
+
+		PopulateCurrentSequence(texts, sequenceDelay);
+
+		//As with basic SetText, we need to clear the reference in-class when we're done
+		currentSequence.OnKill(() => currentSequence = null);
+	}
+
+	/// <summary>
+	/// Animate the text through a series of strings. Fades in and out between each loop. (Loops infinitely)
+	/// </summary>
+	/// <param name="texts">The strings, in order, to display in the UI</param>
+	/// <param name="loopDelay">Time between each string, unfaded</param>
+	public void SetTextOnLoop(List<string> texts, float loopDelay)
+	{
+		//This is our loop version, so be sure to let it know to loop
+		ResetSequence().SetLoops(-1);
+
+		PopulateCurrentSequence(texts, loopDelay);
+	}
+
+	/// <summary>
+	/// Adds the strings in order, with the delay between them
+	/// </summary>
+	/// <param name="texts"></param>
+	/// <param name="delay"></param>
+	void PopulateCurrentSequence(List<string> texts, float delay)
+	{
+		foreach (var item in texts)
+		{
+			currentSequence.AppendCallback(() => textUI.text = item);
+
+			if (texts.IndexOf(item) == 0)
+				currentSequence.Append(textUI.DOFade(1, fadeInTime));
+
+			currentSequence.AppendInterval(delay);
+		}
+	}
+
+	/// <summary>
+	/// Kills any old sequence this tween controller has made and preps a new one (by fading out last text) 
+	/// </summary>
+	/// <returns>The fresh version of currentSequence</returns>
+	Sequence ResetSequence()
+	{
+		if (currentSequence != null && currentSequence.IsPlaying())
+		{
+			currentSequence.Kill();
+		}
+
+		currentSequence = DOTween.Sequence()
+			.Append(textUI.DOFade(0, fadeOutTime));
+
+		return currentSequence;
+	}
+
+	private void Awake()
+	{
+		textUI = GetComponent<TMPro.TextMeshProUGUI>();
+	}
+}
