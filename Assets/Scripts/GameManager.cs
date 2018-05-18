167708a3ee075f5bd66ce819732bb85a5fadd4ac
Alex Ferbrache <alex.ferbrache@gmail.com>
2018-05-17 21:33:59 -0700

    Updating game manager with new player class

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public CardView cardPrefab;
 
-    List<object> _players;
-    private object _currentPlayer;
+    List<Player> _players;
+    private Player _currentPlayer;
 
     // GameManager singleton access
     public static GameManager instance { get; private set; }
@@ -29,13 +30,27 @@ public class GameManager : MonoBehaviour
         // Start game
 	}
 
-    public void EndTurn(object player) {
+    public void EndTurn() {
+        if (_players.Count != 2) {
+            Debug.LogError("Unexpected number of players: " + _players.Count);
+            return;
+        }
+
         // End the current player's turn
-        // Start the next player's turn
+        _currentPlayer.EndTurn();
+
+        // Determine the next player and start their turn
+        if (_currentPlayer == _players.First()) {
+            _currentPlayer = _players.Last();
+        } else {
+            _currentPlayer = _players.First();
+        }
+
+        _currentPlayer.StartTurn();
     }
 
 	private void Update() {
-		// Tick player's turn timer
+		// Tick current player's turn timer
             // If it expires, end their turn
 	}
 }
